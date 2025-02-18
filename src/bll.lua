6038edaced4ea30914c4922ad084e254568040e2
timm <timm@ieee.org>
2025-02-18 13:52:27 -0500

    push

---
+++ src/bll.lua
@@ -0,0 +1,143 @@
+local the,help = {},[[
+nb.lua : Naive Bayes    
+(c) 2025, Tim Menzies <timm@ieee.org>, MIT License  
+   
+OPTIONS:  
+
+      -a acq     xploit or xplore or adapt   = xplout  
+      -d decs    decimal places for printing = 2  
+      -f file    training csv file           = ../test/data/auto93.csv  
+      -g guess   size of guess               = 0.5  
+      -G Guesses max number of guesses       = 100  
+      -k k       low frequency Bayes hack    = 1  
+      -l leaf    min size of tree leaves     = 2
+      -m m       low frequency Bayes hack    = 2  
+      -p p       distance formula exponent   = 2  
+      -r rseed   random number seed          = 1234567891  
+      -s start   where to begin              = 4  
+      -S Stop    where to end                = 32]]
+
+local BIG=1E32
+
+-- ------------------------------------------------------------
+local fmt=string.format
+
+local function o(x)
+  local t    = {}
+  local LIST = function() for _,v in pairs(x) do t[1+#t]= o(v) end end
+  local DICT = function() for k,v in pairs(x) do t[1+#t]= fmt(":%s %s",k,o(v)) end end
+  if type(x) == "number" then return fmt(x//1 == x and "%s" or "%.3g",x) end
+  if type(x) ~= "table"  then return tostring(x) end
+  if #x>0 then LIST() else DICT(); table.sort(t) end
+  return "{" .. table.concat(t, " ") .. "}" end
+
+local function new(klass,object)
+  klass.__index=klass; setmetatable(object,klass); return object end
+
+local function push(t,x) t[1+#t] = x ; return x end
+
+local function coerce(s)
+   local ok,x = pcall( function() return s+0 end )
+   return ok and x or s:match"^%s*(.-)%s*$"  end
+
+local function csv(src)
+  local F = function(s2,z)
+              for s3 in s2:gmatch"([^,]+)" do z[1+#z]=coerce(s3) end
+               return z end
+  src = io.input(src)
+  return function(      s1)
+    s1 = io.read()
+    if s1 then return F(s1,{}) else io.close(src) end end  end
+
+local function main(t,funs,settings)
+  for n,s in pairs(t) do
+    if funs[s] then
+       math.randomseed(settings.rseed)
+       funs[s](t[n+1]) 
+    else for k,_ in pairs(settings) do 
+           if  s == "-"..k:sub(1,1) then
+              settings[k] = coerce(t[n+1])  end end end end end
+
+-- ------------------------------------------------------------
+local Num,Sym,Data,Meta={},{},{},{}
+
+function Num:new(txt,at)
+   return new(Num,{txt=txt or " ", at=at or 0, n=0, 
+                   mu=0, sd=0, m2=0, hi= -BIG, lo= BIG,
+                   goal = tostring(txt):find"-$" and 0 or 1}) end  
+
+function Sym:new(txt, at)
+   return new(Sym, {txt=txt or "", at=at or 0, n=0, has={}, most=0, mode=nil}) end
+
+function Data:new(src)
+   self = new(Data,{rows={}, cols=nil})
+   if   type(src)=="string" 
+   then for   row in csv(src)         do self:add(row) end
+   else for _,row in pairs(src or {}) do self:add(row) end end 
+   return self end
+
+function Meta:new(names)
+   local x,y,all,klass = {}, {}, {}, nil
+   for at,txt in pairs(names) do
+      local col = push(all, (txt:find"^[A-Z]" and Num or Sym):new(txt,at))
+      if not txt:find"X$" then 
+         push(txt:find"[!+-]$" and y or x, col)
+         if txt:find"!$" then klass=col end end end
+   return new(Meta,{x=x,y=y,all=all,klass=klass, names=names}) end 
+             
+-- --------------------------------------------------------------------
+function Data:read(src)
+  src = io.input(src)
+  while true do
+    local line = io.read()
+    if not line then return io.close(src) end
+    local row={}
+    for s in line:gmatch"([^,]+)" do row[1 + #row] = coerce(s) end
+    self:add(row) end end
+
+function Data:add(row)
+   if   self.cols
+   then push(self.rows, self.cols:add(row))
+   else self.cols = Meta:new(row) end end
+
+function Meta:add(row)
+   for _,col in pairs(self.all) do col:add(row[col.at]) end
+   return row end
+
+function Sym:add(x)
+  if x=="?" then return x end
+  self.n = self.n + 1
+  self.has[x] = 1 + (self.has[x] or 0)
+  if self.has[x] > self.most then
+    self.most, self.mode = self.has[x], x end end
+
+function Num:add(n)
+  if n=="?" then return n end
+  self.n  = self.n + 1
+  n       = n + 0 -- ensure we have numbers
+  local d = n - self.mu
+  self.mu = self.mu + d/self.n
+  self.m2 = self.m2 + d*(n - self.mu)
+  self.sd = self.n < 2 and 0 or (self.m2/(self.n - 1))^0.5
+  self.lo = math.min(n, self.lo)
+  self.hi = math.max(n, self.hi) end
+
+-- ------------------------------------------------------
+
+---- ## Actions
+local go={}
+go["-h"] = function(_) print(help) end
+
+go["--the"] = function(_) print(o(the)) end
+
+go["--data"] = function(_) 
+   for _,col in pairs(Data:new(the.file).cols.x) do print(o(col)) end end
+
+-- ## Start
+help:gsub("[-][%S][%s]+([%S]+)[^\n]+= ([%S]+)", function(k,v) the[k]=coerce(v) end)
+
+math.randomseed(the.rseed)
+
+if    pcall(debug.getlocal,4,1) 
+then  return {the=the, Data=Data, Sym=Sym, Num=Num}
+else  main(arg,go,the) end
